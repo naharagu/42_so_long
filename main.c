b750d4bb3eb44c59296181803da70d298b8316e9
naharagu <76852123+naharagu@users.noreply.github.com>
2022-11-13 20:38:56 +0100

    norm

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 20:33:38 by naharagu          #+#    #+#             */
+/*   Updated: 2022/11/13 20:33:39 by naharagu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mlx.h"
 
-int main()
+int	main(void)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500 , 500, "mlx 42");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
 	mlx_loop(mlx_ptr);
 }
